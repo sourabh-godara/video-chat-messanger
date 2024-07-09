0ea190947ecce44bb179542e2d571ab4b645d2b6
Sourabh <37789151+Souravgodara@users.noreply.github.com>
2024-07-09 16:23:48 +0530

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { ThemeToggle } from '@/components/ThemeToogle'
+import React from 'react'
+
+export default function page() {
+  return (
+    <>
+      <main>
+        <h1 className='text-center text-5xl font-semibold'>Next Starter</h1>
+        <ThemeToggle />
+      </main>
+    </>
+  )
+}
