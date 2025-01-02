eaeb5e02e6d860f3aed172d3908d59197f1cd184
Sourabh <37789151+Souravgodara@users.noreply.github.com>
2025-01-02 20:59:54 +0530

    Optimize Codebase

--- apps/web/src/app/(chat)/c/[id]/page.tsx
+++ apps/web/src/app/(chat)/c/[id]/page.tsx
@@ -5,22 +5,19 @@ import ChatHeader from '@/components/chat/ChatHeader'
 import { getUserIdFromSession } from '@/lib'
 
 type params = {
-  params: {
-    id: string
-  }
+  params: Promise<{ id: string }>
 }
 export default async function page({ params }: params) {
-  const chat = await fetchChats(params.id);
+  const receiverId = (await params).id;
+  const chat = await fetchChats(receiverId);
   const userId = await getUserIdFromSession();
   if (!chat) {
     return <p className='m-auto'>Something Went Wrong!</p>
   }
   return (
-    <>
-      <div className="flex-1 flex flex-col">
-        <ChatHeader user={chat.user} />
-        <Chats userId={userId} chat={chat.messages} receiverId={params.id} />
-      </div>
-    </>
+    <div className="flex-1 flex flex-col">
+      <ChatHeader user={chat.user} />
+      <Chats userId={userId} chat={chat.messages} receiverId={receiverId} />
+    </div>
   )
 }
